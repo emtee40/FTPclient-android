98ab6f8d25301f9068fc80dbc2372cf509eb1b10
qwerty287 <ndev@web.de>
2022-05-03 17:24:07 +0200

    Fix unserializable class

--- app/src/main/java/de/qwerty287/ftpclient/ui/files/providers/SFTPFile.kt
+++ app/src/main/java/de/qwerty287/ftpclient/ui/files/providers/SFTPFile.kt
@@ -4,27 +4,17 @@ import net.schmizz.sshj.sftp.FileMode
 import net.schmizz.sshj.sftp.RemoteResourceInfo
 import java.util.*
 
-class SFTPFile(private val file: RemoteResourceInfo) : File {
-    override val name: String
-        get() = file.name
-    override val size: Long
-        get() = file.attributes.size
-    override val user: String
-        get() = file.attributes.uid.toString()
-    override val group: String
-        get() = file.attributes.gid.toString() // TODO get real name
-    override val timestamp: Calendar
-        get() = Calendar.getInstance().apply {
+class SFTPFile(file: RemoteResourceInfo) : File {
+    override val name: String = file.name
+    override val size: Long = file.attributes.size
+    override val user: String = file.attributes.uid.toString()
+    override val group: String = file.attributes.gid.toString() // TODO get real name
+    override val timestamp: Calendar = Calendar.getInstance().apply {
             time = Date(file.attributes.mtime)
         }
-    override val isDirectory: Boolean
-        get() = file.isDirectory
-    override val isFile: Boolean
-        get() = file.isRegularFile
-    override val isSymbolicLink: Boolean
-        get() = file.attributes.type == FileMode.Type.SYMLINK
-    override val isUnknown: Boolean
-        get() = !(isDirectory || isFile || isSymbolicLink)
-    override val link: String?
-        get() = null
+    override val isDirectory: Boolean = file.isDirectory
+    override val isFile: Boolean = file.isRegularFile
+    override val isSymbolicLink: Boolean = file.attributes.type == FileMode.Type.SYMLINK
+    override val isUnknown: Boolean = !(isDirectory || isFile || isSymbolicLink)
+    override val link: String? = null
 }
