acc30c7db1c4854f9ffee986d12d7285853f0185
qwerty287 <ndev@web.de>
2024-01-01 14:10:25 +0100

    Update parameter names

--- app/src/main/java/de/qwerty287/ftpclient/data/AppDatabase.kt
+++ app/src/main/java/de/qwerty287/ftpclient/data/AppDatabase.kt
@@ -25,71 +25,71 @@ abstract class AppDatabase : RoomDatabase() {
                 )
                     .addMigrations(
                         object : Migration(1, 2) {
-                            override fun migrate(database: SupportSQLiteDatabase) {
-                                database.execSQL("ALTER TABLE 'connections' ADD COLUMN 'port' INTEGER NOT NULL DEFAULT 21")
+                            override fun migrate(db: SupportSQLiteDatabase) {
+                                db.execSQL("ALTER TABLE 'connections' ADD COLUMN 'port' INTEGER NOT NULL DEFAULT 21")
                             }
                         },
                         object : Migration(2, 3) {
-                            override fun migrate(database: SupportSQLiteDatabase) {
-                                database.execSQL("CREATE TABLE IF NOT EXISTS 'bookmarks' ('_id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 'title' TEXT NOT NULL, 'directory' TEXT NOT NULL, 'connection' INTEGER NOT NULL)")
+                            override fun migrate(db: SupportSQLiteDatabase) {
+                                db.execSQL("CREATE TABLE IF NOT EXISTS 'bookmarks' ('_id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 'title' TEXT NOT NULL, 'directory' TEXT NOT NULL, 'connection' INTEGER NOT NULL)")
                             }
                         },
                         object : Migration(3, 4) {
-                            override fun migrate(database: SupportSQLiteDatabase) {
-                                database.execSQL("ALTER TABLE 'connections' ADD COLUMN 'secure' INTEGER NOT NULL DEFAULT 0")
+                            override fun migrate(db: SupportSQLiteDatabase) {
+                                db.execSQL("ALTER TABLE 'connections' ADD COLUMN 'secure' INTEGER NOT NULL DEFAULT 0")
                             }
                         },
                         object : Migration(4, 5) {
-                            override fun migrate(database: SupportSQLiteDatabase) {
-                                database.execSQL("CREATE TABLE IF NOT EXISTS 'connections_tmp' ('_id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 'title' TEXT NOT NULL, 'server' TEXT NOT NULL, 'port' INTEGER NOT NULL, 'username' TEXT NOT NULL, 'password' TEXT NOT NULL, 'type' TEXT NOT NULL DEFAULT 'FTP')")
-                                database.execSQL("INSERT INTO 'connections_tmp' ('title', 'server', 'port', 'username', 'password', '_id') SELECT title, server, port, username, password, _id FROM 'connections'")
-                                database.execSQL("DROP TABLE 'connections'")
-                                database.execSQL("ALTER TABLE 'connections_tmp' RENAME TO 'connections'")
+                            override fun migrate(db: SupportSQLiteDatabase) {
+                                db.execSQL("CREATE TABLE IF NOT EXISTS 'connections_tmp' ('_id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 'title' TEXT NOT NULL, 'server' TEXT NOT NULL, 'port' INTEGER NOT NULL, 'username' TEXT NOT NULL, 'password' TEXT NOT NULL, 'type' TEXT NOT NULL DEFAULT 'FTP')")
+                                db.execSQL("INSERT INTO 'connections_tmp' ('title', 'server', 'port', 'username', 'password', '_id') SELECT title, server, port, username, password, _id FROM 'connections'")
+                                db.execSQL("DROP TABLE 'connections'")
+                                db.execSQL("ALTER TABLE 'connections_tmp' RENAME TO 'connections'")
                             }
                         },
                         object : Migration(3, 5) {
-                            override fun migrate(database: SupportSQLiteDatabase) {
-                                database.execSQL("ALTER TABLE 'connections' ADD COLUMN 'type' TEXT NOT NULL DEFAULT 'FTP'")
+                            override fun migrate(db: SupportSQLiteDatabase) {
+                                db.execSQL("ALTER TABLE 'connections' ADD COLUMN 'type' TEXT NOT NULL DEFAULT 'FTP'")
                             }
                         },
                         object : Migration(5, 6) {
-                            override fun migrate(database: SupportSQLiteDatabase) {
-                                database.execSQL("ALTER TABLE 'connections' ADD COLUMN 'implicit' INTEGER NOT NULL DEFAULT 0")
+                            override fun migrate(db: SupportSQLiteDatabase) {
+                                db.execSQL("ALTER TABLE 'connections' ADD COLUMN 'implicit' INTEGER NOT NULL DEFAULT 0")
                             }
                         },
                         object : Migration(6, 7) {
-                            override fun migrate(database: SupportSQLiteDatabase) {
-                                database.execSQL("ALTER TABLE 'connections' ADD COLUMN 'utf8' INTEGER NOT NULL DEFAULT 0")
+                            override fun migrate(db: SupportSQLiteDatabase) {
+                                db.execSQL("ALTER TABLE 'connections' ADD COLUMN 'utf8' INTEGER NOT NULL DEFAULT 0")
                             }
                         },
                         object : Migration(7, 8) {
-                            override fun migrate(database: SupportSQLiteDatabase) {
-                                database.execSQL("ALTER TABLE 'connections' ADD COLUMN 'passive' INTEGER NOT NULL DEFAULT 0")
+                            override fun migrate(db: SupportSQLiteDatabase) {
+                                db.execSQL("ALTER TABLE 'connections' ADD COLUMN 'passive' INTEGER NOT NULL DEFAULT 0")
                             }
                         },
                         object : Migration(8, 9) {
-                            override fun migrate(database: SupportSQLiteDatabase) {
-                                database.execSQL("ALTER TABLE 'connections' ADD COLUMN 'start_directory' TEXT NOT NULL DEFAULT ''")
+                            override fun migrate(db: SupportSQLiteDatabase) {
+                                db.execSQL("ALTER TABLE 'connections' ADD COLUMN 'start_directory' TEXT NOT NULL DEFAULT ''")
                             }
                         },
                         object : Migration(9, 10) {
-                            override fun migrate(database: SupportSQLiteDatabase) {
-                                database.execSQL("ALTER TABLE 'connections' ADD COLUMN 'public_key' INTEGER NOT NULL DEFAULT 0")
+                            override fun migrate(db: SupportSQLiteDatabase) {
+                                db.execSQL("ALTER TABLE 'connections' ADD COLUMN 'public_key' INTEGER NOT NULL DEFAULT 0")
                             }
                         },
                         object : Migration(10, 11) {
-                            override fun migrate(database: SupportSQLiteDatabase) {
-                                database.execSQL("ALTER TABLE 'connections' ADD COLUMN 'private_data' INTEGER NOT NULL DEFAULT 1")
+                            override fun migrate(db: SupportSQLiteDatabase) {
+                                db.execSQL("ALTER TABLE 'connections' ADD COLUMN 'private_data' INTEGER NOT NULL DEFAULT 1")
                             }
                         },
                         object : Migration(11, 12) {
-                            override fun migrate(database: SupportSQLiteDatabase) {
-                                database.execSQL("ALTER TABLE 'connections' ADD COLUMN 'saf_integration' INTEGER NOT NULL DEFAULT 0")
+                            override fun migrate(db: SupportSQLiteDatabase) {
+                                db.execSQL("ALTER TABLE 'connections' ADD COLUMN 'saf_integration' INTEGER NOT NULL DEFAULT 0")
                             }
                         },
                         object : Migration(12, 13) {
-                            override fun migrate(database: SupportSQLiteDatabase) {
-                                database.execSQL("ALTER TABLE 'connections' RENAME COLUMN 'public_key' TO 'private_key';")
+                            override fun migrate(db: SupportSQLiteDatabase) {
+                                db.execSQL("ALTER TABLE 'connections' RENAME COLUMN 'public_key' TO 'private_key';")
                             }
                         }
                     )
